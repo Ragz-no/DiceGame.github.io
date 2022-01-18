4f99efa6d3c5c5b6931c8f19e7a48e84993356d3
Ram <70691805+Ragz-no@users.noreply.github.com>
2022-01-18 09:12:40 +0100

    Solution to dice game images
    solution to refresh dice game images randomly

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 //RNG for dice img
-    var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-    var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-
-   
-    document.getElementById("img1").setAttribute = ("src", "./images/dice" + randomNumber1 + ".png");
-    document.getElementById("img2").setAttribute = ("src", "images/dice" + randomNumber2 + ".png");
+    var randomNumber1 = Math.ceil(Math.random() * 6);
+    var randomNumber2 = Math.ceil(Math.random() * 6);
+    
+    document.querySelector(".dice .img1").setAttribute("src", `./images/dice${randomNumber1}.png`);
+    document.querySelector(".dice .img2").setAttribute("src", `./images/dice${randomNumber2}.png`);
 
 // hvis den ene terningen er høyere enn den andre
-    if (randomNumber1 === randomNumber2) {
+    if (randomNumber1 == randomNumber2) {
     document.getElementById("result-text").innerHTML = "Draw!";
     
     } else if (randomNumber2 > randomNumber1) {
-    document.getElementById("result-text").innerHTML = "Player 1 Wins!";
+    document.getElementById("result-text").innerHTML = "Player 2 Wins!";
     } 
     
     else if (randomNumber2 < randomNumber1) {
-    document.getElementById("result-text").innerHTML = " Player 2 Wins!"
+    document.getElementById("result-text").innerHTML = " Player 1 Wins!"
     }
     
     else {
